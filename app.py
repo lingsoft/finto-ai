3b8c4534fc9ac0e015257997029df4662e28d021
Jarmo Hemminki <87070956+jarmo-lls@users.noreply.github.com>
2022-02-18 13:17:43 +0200

    Simplify response structure and clean code
    Remove extra level of texts from the response. Top-level list is now
    set of alternatives. Remove structuredText endpoint. Use defaults in
    get calls. Rename from Annif to Finto.

--- app.py
+++ app.py
@@ -1,50 +1,29 @@
 import utils
 from elg import FlaskService
-from elg.model import Failure
-from elg.model import TextsResponse, TextRequest, StructuredTextRequest
+from elg.model import Failure, TextsResponse, TextRequest
 from elg.model.base import StandardMessages
 
 
-class Annif(FlaskService):
+class Finto(FlaskService):
 
     def process_text(self, request: TextRequest):
-
         text = request.content
         project_id = 'yso-fi'
-        limit = 10
+        limit = 5
         threshold = 0
-        
+
         if request.params:
-            project_id = request.params.get('project_id', 'yso-fi')
-            limit = request.params.get('limit', 2)
-            threshold = request.params.get('threshold', 0)
+            project_id = request.params.get('project_id', project_id)
+            limit = request.params.get('limit', limit)
+            threshold = request.params.get('threshold', threshold)
         try:
             res = utils.handle_text(project_id, text, limit, threshold)
         except Exception as err:
-          detail = {'server error': str(err) }
-          error = StandardMessages.generate_elg_service_internalerror(detail=detail)
-          return Failure(errors=[error])
-        return TextsResponse(texts=[res])
-
-    # def process_structured_text(self, request: StructuredTextRequest):
-      
-    #     texts = request.texts
-    #     res = []
-    #     try:
-    #       for i, text in enumerate(texts):
-    #           project_id, limit, threshold = 'yso-fi', 2, 0
-    #           content = text.content
-    #           if text.features:
-    #             project_id = text.features.get('project_id', 'yso-fi')
-    #             limit = text.features.get('limit', 2)
-    #             threshold = text.features.get('threshold', 0)
-    #           res.append(utils.handle_text(project_id, content, limit, threshold))
-    #     except Exception as err:
-    #       detail = {'server error': str(err) }
-    #       error = StandardMessages.generate_elg_service_internalerror(detail=detail)
-    #       return Failure(errors=[error])
-    #     return TextsResponse(texts=res)
+            detail = {'server error': str(err)}
+            error = StandardMessages.generate_elg_service_internalerror(detail=detail)
+            return Failure(errors=[error])
+        return TextsResponse(texts=res)
 
 
-flask_service = Annif("annif")
+flask_service = Finto("finto")
 app = flask_service.app
--- utils.py
+++ utils.py
@@ -1,14 +1,21 @@
 import requests
 from elg.model import TextsResponseObject
-import json
+
 
 base_url = 'https://ai.finto.fi/v1/projects/'
+
+
 def handle_text(project_id, text, limit, threshold):
-  
-  payload = {'text': text, 'limit': limit, 'threshold': threshold}
-  res = requests.post(f'{base_url}{project_id}/suggest', data=payload)
-  data = res.json()
-  texts = [ {'role': 'alternative', 'content': obj['label'], 'score': obj['score'],
-          'features': {k:v for k,v in obj.items() if k != 'label' and k!= 'score'} } 
-          for obj in data['results'] ]
-  return TextsResponseObject(texts=texts)
+    payload = {'text': text, 'limit': limit, 'threshold': threshold}
+    res = requests.post(f'{base_url}{project_id}/suggest', data=payload)
+    data = res.json()
+    texts_response = []
+    for obj in data["results"]:
+        texts_response.append(TextsResponseObject(
+            role='alternative',
+            content=obj['label'],
+            score=obj['score'],
+            features={k: v for k, v in obj.items()
+                      if k != 'label' and k != 'score'})
+        )
+    return texts_response
